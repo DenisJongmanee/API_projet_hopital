9e933a541aad49f3a749c0d7565751374c902118
DenisJongmanee <jongmanee.denis@gmai.com>
2022-04-11 15:51:45 +0200

    api new repo

---
+++ classes/patient.py
@@ -0,0 +1,6 @@
+class Patient:
+    def __init__(self, nom, prenom, date):
+        self.nom = nom
+        self.prenom = prenom
+        self.date = date
+#str nom et prenom/date date naissance, date
---
+++ classes/service.py
@@ -0,0 +1,5 @@
+class Service:
+    def __init__(self, nom, zone):
+        self.nom = nom
+        self.zone = zone
+#str nom et zone, zone géographique
