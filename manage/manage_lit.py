1da18fb86d413fd0ef18ddd2f204e60d5f0b08d3
Degeur <pierrick.bauffe@epsi.fr>
2022-04-21 16:51:35 +0200

    changements api

--- manage/manage_lit.py
+++ manage/manage_lit.py
@@ -9,15 +9,29 @@ class Manage_lit:
 
     def afficher_liste_lit(self):
         # methode pour afficher toutes les chambres
-        instructionBDD = "SELECT numero_lit, id_sejour, numero_chambre FROM chambre INNER JOIN sejour_chambre ON chambre.id_chambre=sejour_chambre.num_chambre INNER JOIN sejour ON sejour_chambre.num_sejour=sejour.id_sejour RIGHT JOIN lit ON sejour.num_lit=lit.id_lit"
+        instructionBDD = "SELECT * FROM Lit"
+        self.curseurBDD.execute(instructionBDD)
+
+        lits = self.curseurBDD.fetchall()
+
+        retour = []
+        print(lits)
+        for lit in lits:
+            retour.append({"id":lit[0], "num_lit":lit[1]})
+        return retour
+
+
+    def afficher_liste_lit_cs(self):
+        # methode pour afficher toutes les chambres
+        instructionBDD = "SELECT numero_lit, numero_chambre FROM chambre INNER JOIN sejour_chambre ON chambre.id_chambre=sejour_chambre.num_chambre INNER JOIN sejour ON sejour_chambre.num_sejour=sejour.id_sejour RIGHT JOIN lit ON sejour.num_lit=lit.id_lit"
         self.curseurBDD.execute(instructionBDD)
         lits = self.curseurBDD.fetchall()
         retour = []
         print(lits)
         for lit in lits:
             if lit[1] == None :
-                retour.append({"num_lit":lit[0], "id_sejour":lit[1], "num_chambre":lit[2], "disponible":"oui"})
+                retour.append({"NumeroLit":lit[0], "NumeroChambre":lit[1], "Occupation":"non"})
             else :
-                retour.append({"num_lit":lit[0], "id_sejour":lit[1], "num_chambre":lit[2], "disponible":"non"})
+                retour.append({"NumeroLit":lit[0], "NumeroChambre":lit[1], "Occupation":"oui"})
 
         return retour
